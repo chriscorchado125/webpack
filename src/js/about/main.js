c5e01917de8bdcef5ab1c68aaafbd8136a4cffd0
chriscorchado125 <chris.corchado125@gmail.com>
2020-11-01 10:10:22 -0500

    cleanup

--- src/js/about/main.js
+++
@@ -1,9 +0,0 @@
-import regeneratorRuntime from "regenerator-runtime";
-
-require('./../css/chriscorchado.css')
-require('./../css/pages/home.css')
-require('./../css/header/profile.css')
-
-require('./../js/utilities.js')
-require('./../js/data.js')
-require('./../js/chriscorchado.js')
